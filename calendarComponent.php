9a38e066a92c46c2737bb444e2573c6584840c68
Arthur McBain <mcbain.asm@gmail.com>
2018-11-06 19:30:47 -0800

    Rearrange and restyle the components in the calendar event items. Also make it only show the next two months from the current date, not year.

--- calendarComponent.php
+++ calendarComponent.php
@@ -29,12 +29,14 @@ function getClient() {
 $client = getClient();
 $service = new Google_Service_Calendar($client);
 
+$time = time();
 $optParams = array(
   'maxResults' => 100,
   'orderBy' => 'startTime',
   'singleEvents' => TRUE,
-  'timeMin' => date('c', time()-1800/*30min*/ ),
-  'timeMax' => date('c', time()+31536000/*year*/),
+  'timeMin' => date('c', $time-1800/*30min*/ ),
+//  'timeMax' => date('c', time()+31536000/*year*/),
+  'timeMax' => date('c', $time+5270400/*~2 months (61 days)*/),
 );
 $events = $service->events->listEvents(CALENDAR, $optParams)->getItems();
 ?>
@@ -45,23 +47,20 @@ $events = $service->events->listEvents(CALENDAR, $optParams)->getItems();
     }
     
     .event {
-      font-family: sans-serif;
-      margin-top: 10px;
-      background: #ddd;
+      margin-bottom: 1.25em;
       
       display: flex;
       flex-direction: row;
       
       color: black;
     }
-    
-    .event:nth-child(odd) {
-      background: #eee;
+
+    .event h4 {
+      margin-top: 0;
     }
     
     .event .summary {
-      margin: 0;
-      padding: 5px 0 0 0;
+      margin-bottom: .3125em;
     }
     
     .event .summary a {
@@ -70,10 +69,12 @@ $events = $service->events->listEvents(CALENDAR, $optParams)->getItems();
     
     .event .register {
       float: right;
-      padding: 10px;
+      font-size: 1.25em;
+      padding: .3em 1em;
       background: #f99d33;
-      color: black;
-      font-weight: bold;
+      color: #1e4897;
+      text-decoration: none;
+      border-radius: 5px;
     }
     
     .event .when {
@@ -83,10 +84,18 @@ $events = $service->events->listEvents(CALENDAR, $optParams)->getItems();
       color: #555
     }
     
-    .event .description {
+    .event .shortdesc, .event .description {
       white-space: pre-wrap;
-      padding: 0 5px 0 0;
-      display: table;
+      padding: 0 .3125em 0 0;
+    }
+
+    .event .description > p:first-child,
+    .event .shortdesk > p:first-child {
+      margin-top: 0;
+    }
+
+    .event .details {
+      display: none;
     }
     
     .event .location {
@@ -94,31 +103,33 @@ $events = $service->events->listEvents(CALENDAR, $optParams)->getItems();
     }
     
     .event .price {
-      position: absolute;
-      right: 0;
-      bottom: 0;
-      
+      margin-bottom: 1em;
       font-size: small;
       font-weight: bold;
       padding-right: 5px;
-      text-align: right;
       white-space: pre-wrap;
     }
     
     .event-content {
       flex: 1 1 auto;
       position: relative;
+      overflow: hidden;
+      background: white;
+      padding: .625em;
+      border-radius: 5px;
     }
     
     .calendar-date {
+      align-self: flex-start;
       flex: 0 0 auto;
       
       box-sizing: border-box;
       background-color:#FFFFFF;
-      border:1px solid #CCCCCC;
-      /*margin: 0 auto;*/
-      padding: 5px;
-      width: 120px;
+      border-radius: 5px;
+
+      padding-bottom: .625em;
+      width: 80px;
+      overflow: hidden;
       
       color:#D0722C;
       font-family:Helvetica, Arial, sans-serif;
@@ -133,10 +144,8 @@ $events = $service->events->listEvents(CALENDAR, $optParams)->getItems();
     .calendar-date .month {
       background-color:#f99d33;
       color:#FFFFFF;
-      font-size:16px;
       line-height:100%;
-      padding-top:10px;
-      padding-bottom:10px;
+      padding: .625em 0;
     }
     
     .calendar-date .date {
@@ -151,6 +160,20 @@ $events = $service->events->listEvents(CALENDAR, $optParams)->getItems();
       line-height:100%;
       padding-top:0px;
       padding-bottom:0px;
+    }
+
+    .event-content .more {
+      border-radius: .5em;
+      display: block;
+      background: #f99d33;
+      height: 1em;
+      line-height: 1em;
+      width: 2em;
+      text-align: center;
+      font-weight: bold;
+      color: #1e4897;
+      cursor: pointer;
+      margin: 1em auto;
     }
   </style>
   <div>
@@ -180,44 +203,50 @@ if (count($events) == 0) {
     $price = str_replace("</p><p>","\n",$price);
     
     
-    
+    $hasdesc = strlen($event->description) > 0;
+    $hasmore = FALSE;
+
     $descshort = preg_split('/\n|\r/', $event->description, -1, PREG_SPLIT_NO_EMPTY);
     $descshort = $descshort[0];
     $descshort = substr($descshort, 0, 200);
     if( strlen($descshort) != strlen($event->description) ) {
-      
-    $descshort = preg_replace("~[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]~",'<a href="$0">$0</a>',$descshort);
-      $descshort .= "...";
+      $descshort = preg_replace("~[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]~",'<a href="$0">$0</a>',$descshort);
+      $descshort .= "…";
+      $hasmore = TRUE;
     } else {
       $descshort = preg_replace("~[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]~",'<a href="$0">$0</a>',$descshort);
     }
     
-    $clazz =
+    $cssClass =
       ($isNew?"new":"") +
       ($isUpdated?"updated":"") +
       ($isCancelled?"cancelled":"") +
       "";
-      
-    
 ?>
 
-<div class="event <?=$clazz?>">
+<div class="event <?=$cssClass?>">
   <div class="calendar-date">
-		<div class="month"><?=(new DateTime($start))->format("F")?></div>
-		<div class="date"><?=(new DateTime($start))->format("d")?></div>
-		<div class="day"><?=(new DateTime($start))->format("D")?></div>
-	</div>
-	<div class="event-content">
-  	<?php if($event->location) { ?>
-      <div class="location"><img title="<?=$event->location?>" src="https://maps.googleapis.com/maps/api/staticmap?style=feature:poi|element:labels.text|visibility:off&markers=color:blue%7C<?=urlencode($event->location)?>&zoom=17&size=200x100&key=<?=$MAPS_BROWSER_KEY ?>" /></div>
-    <?php } ?>
-  	<?=$storeLink?"":"<!--"?><a class="register" href="<?=$storeLink?>">Register</a><?=$storeLink?"":"-->"?>
-  	<h4 class="summary"><a href="<?=$event->htmlLink?>"><?=$event->summary?></a></h4>
-    
+    <div class="month"><?=(new DateTime($start))->format("M")?></div>
+    <div class="date"><?=(new DateTime($start))->format("d")?></div>
+    <div class="day"><?=(new DateTime($start))->format("D")?></div>
+  </div>
+  <div class="event-content">
+    <?php if ($storeLink) { ?><a class="register" href="<?=$storeLink?>">Register</a><?php } ?>
+    <h4 class="summary"><a href="<?=$event->htmlLink?>"><?=$event->summary?></a></h4>
     <div class="when"><?=(new DateTime($start))->format("g:i a")?> - <?=(new DateTime($end))->format("g:i a")?></div>
-  	<div class="description"><?=$descshort?></div>
     <div class="price"><?=$price?></div>
-    <div style="clear:both;"></div>
+    <?php if($hasdesc) { ?>
+    <div class="shortdesc"><?=$descshort?></div>
+    <?php } ?>
+    <?php if($event->location || $hasmore) { ?>
+    <span class="more" title="Click to show more details" onClick="if (event.target.previousElementSibling.style.display) { event.target.previousElementSibling.style.display = ''; event.target.nextElementSibling.style.display = ''; } else { event.target.previousElementSibling.style.display = 'none'; event.target.nextElementSibling.style.display = 'block'; }">&#8943;</span>
+    <div class="details">
+      <?php if($event->location) { ?>
+      <div class="location"><img title="<?=$event->location?>" src="https://maps.googleapis.com/maps/api/staticmap?style=feature:poi|element:labels.text|visibility:off&markers=color:blue%7C<?=urlencode($event->location)?>&zoom=17&size=200x100&key=<?=$MAPS_BROWSER_KEY ?>" /></div>
+      <?php } ?>
+      <div class="description"><?=$event->description?></div>
+    </div>
+    <?php } ?>
   </div>
 </div>
 
